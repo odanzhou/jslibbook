7eb2a27a5193026af7b40b222fa188d754d881b3
odanzhou <odanzhou@gmail.com>
2023-01-29 17:18:10 +0800

    feat: rollup esm && aio 的打包

---
+++ config/rollup.config.aio.js
@@ -0,0 +1,16 @@
+const nodeResolve = require('rollup-plugin-node-resolve')
+
+module.exports = {
+  input: 'index.js', // 'src/index.js'
+  output: {
+    file: 'dist/index.aio.js',
+    format: 'umd',
+    name: 'clone'
+  },
+  plugins: [
+    nodeResolve({
+      main: true,
+      extensions: ['.js']
+    })
+  ]
+}
