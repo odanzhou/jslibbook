21d841c608c8f6ab5efe264971895893dc1cf41a
odanzhou <odanzhou@gmail.com>
2023-01-29 16:43:45 +0800

    feat: webpack 打包

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path')
+
+module.exports = {
+  mode: 'development', // 'production'
+  entry: './index.js',
+  output: {
+    filename: 'index.js',
+    path: path.resolve(__dirname, 'dist')
+  }
+}
