e555274dcad4b537cc15eb0f0d2e91ed7c921b7e
odanzhou <odanzhou@gmail.com>
2023-04-20 01:22:07 +0800

    feat: 打包为es

--- config/rollup.config.esm.js
+++ config/rollup.config.esm.js
@@ -4,8 +4,8 @@ const Rollup = require('rollup')
  * @type { Rollup.RollupWatchOptions}
  */
 const config = {
-  // input: 'src/index.js',
-  input: 'src/commonjs.index.js', // 测试commonjs转esmodule
+  input: 'src/index.js',
+  // input: 'src/commonjs.index.js', // 测试commonjs转esmodule，不行
   output: {
     file: 'dist/index.esm.js',
     format: 'es'
