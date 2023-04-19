3fc938cc3fdcf4a36f3abaa837057a31e9cc848d
odanzhou <odanzhou@gmail.com>
2023-04-20 01:20:52 +0800

    feat: 尝试commonjs打包为es,结果为不行

--- config/rollup.config.esm.js
+++ config/rollup.config.esm.js
@@ -4,7 +4,8 @@ const Rollup = require('rollup')
  * @type { Rollup.RollupWatchOptions}
  */
 const config = {
-  input: 'src/index.js',
+  // input: 'src/index.js',
+  input: 'src/commonjs.index.js', // 测试commonjs转esmodule
   output: {
     file: 'dist/index.esm.js',
     format: 'es'
