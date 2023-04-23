a3305921db1bc8155711a5043bbdf6887a76e811
odanzhou <odanzhou@gmail.com>
2023-04-23 22:54:57 +0800

    feat: 尝试单元测试

--- test/test.js
+++ test/test.js
@@ -7,6 +7,22 @@ describe('单元测试', () => {
     it('hello', () => {
       // 断言
       expect(1).to.equal(1)
+      // 相等或者不想等
+      expect(4 + 5).to.be.equal(9)
+      expect(4 + 5).to.not.be.equal(10)
+      // 布尔值
+      expect('everything').to.be.ok;
+      expect('').to.not.be.ok;
+      // include
+      expect([1, 2, 3]).to.contain(1) // include 不能用
+      expect('foobar').to.contain('foo')
+      expect({foo: 'bar', hello: 'universe'}).to.include.keys('foo');
+      // empty
+      expect([]).to.be.empty;
+      expect('').to.be.empty;
+      expect({}).to.be.empty;
+      // match
+      expect('foobar').to.match(/^foo/)
     })
   })
 })
