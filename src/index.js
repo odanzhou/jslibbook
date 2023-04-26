e0872ddf4e6d9c2f6bf7eb6645dbc11c2fc87b3b
odanzhou <odanzhou@gmail.com>
2023-04-26 23:04:30 +0800

    feat: 增加协议

--- src/index.js
+++ src/index.js
@@ -1,14 +1,5 @@
-import  { aaa, clone } from './clone'
-
-let a = { c: 1}
-let b = clone(a) // 深拷贝
-
-a.c = 2 // 对变量 a 的修改不会影响到变量 b
-
-console.log(a.c) // 输出 2
-console.log(b.c) // 输出 1
+import  { clone } from './clone'
 
 export {
-  clone,
-  aaa,
+  clone
 }
